600003cee6cadff6ffae7f932a882356711ff41d
bellusfennec <bellusfennec@gmail.com>
2023-04-05 11:50:13 +0300

    рефакторинг

--- src/app/components/users.jsx
+++ src/app/components/users.jsx
@@ -7,67 +7,72 @@ const Users = () => {
     setUsers((prev) => prev.filter((user) => user._id !== userId));
   };
   const handlePhrase = (number) => {
-    let text;
-    let classes = "badge m-2";
-    if (number) {
-      const humans = number <= 4 ? "человека" : "человек";
-      classes += " bg-primary";
-      text = `${number} ${humans} тусанет с тобой сегодня`;
-    } else {
-      classes += " bg-danger";
-      text = "Никто с тобой не тусанет";
+    if (!number) {
+      return (
+        <span className="badge m-2 bg-danger">Никто с тобой не тусанет</span>
+      );
     }
-    return <span className={classes}>{text}</span>;
+    if (number <= 4) {
+      return (
+        <span className="badge m-2 bg-primary">
+          {number} человека тусанет с тобой сегодняет
+        </span>
+      );
+    }
+    if (number) {
+      return (
+        <span className="badge m-2 bg-primary">
+          {number} человек тусанет с тобой сегодня
+        </span>
+      );
+    }
   };
-
+  if (!users?.length) {
+    return <h2>{handlePhrase(0)}</h2>;
+  }
   return (
     <>
       <h2>{handlePhrase(users.length)}</h2>
-      {users.length ? (
-        <table className="table">
-          <thead>
-            <tr>
-              <th scope="col">Имя</th>
-              <th scope="col">Качества</th>
-              <th scope="col">Профессия</th>
-              <th scope="col">Встретился, раз</th>
-              <th scope="col">Оценка</th>
-              <th></th>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Имя</th>
+            <th scope="col">Качества</th>
+            <th scope="col">Профессия</th>
+            <th scope="col">Встретился, раз</th>
+            <th scope="col">Оценка</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user) => (
+            <tr key={user._id}>
+              <td>{user.name}</td>
+              <td>
+                {user.qualities.map((quality) => (
+                  <span
+                    key={quality._id}
+                    className={`badge bg-${quality.color} me-2`}
+                  >
+                    {quality.name}
+                  </span>
+                ))}
+              </td>
+              <td>{user.profession.name}</td>
+              <td>{user.completedMeetings}</td>
+              <td>{user.rate}/5</td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(user._id)}
+                >
+                  Delete
+                </button>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {users &&
-              users.map((user) => (
-                <tr key={user._id}>
-                  <td>{user.name}</td>
-                  <td>
-                    {user.qualities.map((quality) => (
-                      <span
-                        key={quality._id}
-                        className={`badge bg-${quality.color} me-2`}
-                      >
-                        {quality.name}
-                      </span>
-                    ))}
-                  </td>
-                  <td>{user.profession.name}</td>
-                  <td>{user.completedMeetings}</td>
-                  <td>{user.rate}/5</td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleDelete(user._id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-          </tbody>
-        </table>
-      ) : (
-        ""
-      )}
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
