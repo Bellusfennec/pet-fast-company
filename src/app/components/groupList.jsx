9ccfdaca663d16342c7777b26011c42823d90cfa
Bellusfennec <bellusfennec@gmail.com>
2023-04-29 16:55:45 +0300

    refactor(groupList): :recycle:

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -9,22 +9,25 @@ function GroupList({
   valueProperty,
   contentProperty
 }) {
-  if (!Array.isArray(items)) {
-    items = Object.keys(items).map((key) => ({ key, ...items[key] }));
-  }
+  items = !Array.isArray(items)
+    ? Object.keys(items).map((key) => ({ key, ...items[key] }))
+    : items;
 
   return (
     <ul className="list-group">
-      {Object.keys(items).map((item) => (
+      {items.map((item) => (
         <li
-          key={items[item][valueProperty]}
-          onClick={() => onItemSelect(items[item])}
+          key={item[valueProperty]}
+          onClick={() => onItemSelect(item)}
           role="button"
           className={
-            "list-group-item" + (items[item] === selectedItem ? " active" : "")
+            "list-group-item" +
+            (item[valueProperty] === selectedItem?.[valueProperty]
+              ? " active"
+              : "")
           }
         >
-          {items[item][contentProperty]}
+          {item[contentProperty]}
         </li>
       ))}
     </ul>
