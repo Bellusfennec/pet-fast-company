21b1965733cd92d37b0726ce4210415470d60e21
bellusfennec <bellusfennec@gmail.com>
2023-05-10 16:29:59 +0300

    feat:

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,8 +1,23 @@
 /* eslint-disable no-unused-vars */
 import React from "react";
 import UsersList from "./components/usersList";
+import { Switch, Route } from "react-router-dom";
+import Main from "./layouts/main";
+import Users from "./layouts/users";
+import Login from "./layouts/login";
+import NavBar from "./components/navBar";
 
 const App = () => {
-  return <UsersList />;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route path="/users" component={Users} />
+      </Switch>
+      <NavBar />
+      <UsersList />
+    </>
+  );
 };
 export default App;
