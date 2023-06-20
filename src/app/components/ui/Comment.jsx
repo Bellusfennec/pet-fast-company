87a29d309738c02e5c52d9ce61d4f51cedf72250
Bellusfennec <bellusfennec@gmail.com>
2023-06-20 06:41:43 +0300

    refactor:

--- src/app/components/ui/Comment.jsx
+++ src/app/components/ui/Comment.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import API from "../../api";
 
 const Comment = ({ comment, onRemove }) => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
   console.log("comm");
 
   const date = (date) => {
@@ -10,42 +14,55 @@ const Comment = ({ comment, onRemove }) => {
     return date;
   };
 
+  useEffect(() => {
+    setLoading(true);
+    API.users
+      .getById(comment.userId)
+      .then((data) => setUser(data))
+      .finally(() => setLoading(false));
+  }, []);
+
   return (
     <div className="bg-light card-body mb-3">
       <div className="row">
         <div className="col">
-          <div key={comment._id} className="d-flex flex-start">
-            <img
-              src={`https://avatars.dicebear.com/api/avataaars/${(
-                Math.random() + 1
-              )
-                .toString(36)
-                .substring(7)}.svg`}
-              className="rounded-circle shadow-1-strong me-3"
-              alt="avatar"
-              width="65"
-              height="65"
-            />
-            <div className="flex-grow-1 flex-shrink-1">
-              <div className="mb-4">
-                <div className="d-flex justify-content-between align-items-center">
-                  <p className="mb-1">
-                    {comment.userId}
-                    {" - "}
-                    <span className="small">{date(comment.created_at)}</span>
-                  </p>
-                  <button
-                    onClick={() => onRemove(comment._id)}
-                    type="button"
-                    className="btn btn-sm text-primary d-flex align-items-center"
-                  >
-                    <i className="bi bi-x-lg"></i>
-                  </button>
+          {loading && (
+            <div className="d-flex align-items-center">Загрузка...</div>
+          )}
+          {!loading && (
+            <div key={comment._id} className="d-flex flex-start">
+              <img
+                src={`https://avatars.dicebear.com/api/avataaars/${(
+                  Math.random() + 1
+                )
+                  .toString(36)
+                  .substring(7)}.svg`}
+                className="rounded-circle shadow-1-strong me-3"
+                alt="avatar"
+                width="65"
+                height="65"
+              />
+              <div className="flex-grow-1 flex-shrink-1">
+                <div className="mb-4">
+                  <div className="d-flex justify-content-between align-items-center">
+                    <p className="mb-1">
+                      {user.name}
+                      {" - "}
+                      <span className="small">{date(comment.created_at)}</span>
+                    </p>
+                    <button
+                      onClick={() => onRemove(comment._id)}
+                      type="button"
+                      className="btn btn-sm text-primary d-flex align-items-center"
+                    >
+                      <i className="bi bi-x-lg"></i>
+                    </button>
+                  </div>
+                  <p className="small mb-0">{comment.content}</p>
                 </div>
-                <p className="small mb-0">{comment.content}</p>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
