2b8b413254aee54d1638ee8abc22f653cbcb917b
bellusfennec <bellusfennec@gmail.com>
2023-09-04 16:34:21 +0300

    refactor:

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const chalk = require("chalk");
+const config = require("config");
+const initDatabase = require("./startUp/initDatabase");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = config.get("port") ?? 8080;
+
+// if (process.env.NODE_ENV === "production") {
+//   console.log("Production");
+// } else {
+//   console.log("Development");
+// }
+
+async function start() {
+  try {
+    mongoose.connection.once("open", () => {
+      initDatabase();
+    });
+    await mongoose.connect(config.get("mongoUri"));
+    console.log(chalk.green(`Mongo DB connected.`));
+    app.listen(PORT, () =>
+      console.log(chalk.green(`Server has been started on PORT ${PORT}...`))
+    );
+  } catch (e) {
+    console.log(chalk.red(e.message));
+    process.exit(1);
+  }
+}
+
+start();
