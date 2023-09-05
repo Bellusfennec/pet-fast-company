775b7140b4bdb2a65a3141b556b05ab00454ccd7
bellusfennec <bellusfennec@gmail.com>
2023-09-05 08:06:15 +0300

    refactor:

--- server/startUp/initDatabase.js
+++
@@ -1,7 +0,0 @@
-// 1. У любого пользователя будет как минимум в БД qualities & professions
-// 2. Они равны mock данным
-
-const professionMock = require("../mock/professions.json");
-const qualitiesMock = require("../mock/qualities.json");
-
-module.exports = () => {};
